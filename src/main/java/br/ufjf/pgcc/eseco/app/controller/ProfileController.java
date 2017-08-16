0349be36c881e4817cd7dc5aecb38484360204ec
leoap86 <leoaguiarpereira@gmail.com>
2017-08-16 07:16:29 -0300

    * Setup users and agents registration #3

--- src/main/java/br/ufjf/pgcc/eseco/app/controller/ProfileController.java
+++ src/main/java/br/ufjf/pgcc/eseco/app/controller/ProfileController.java
@@ -1,19 +1,10 @@
 package br.ufjf.pgcc.eseco.app.controller;
 
 import br.ufjf.pgcc.eseco.common.controller.CommonController;
-import br.ufjf.pgcc.eseco.domain.model.uac.User;
 import br.ufjf.pgcc.eseco.domain.service.uac.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class ProfileController extends CommonController {
